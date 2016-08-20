fe44a40fa653e08562d21a639218412571eaf601
Greg Smith <crashingwaves@fea.st>
2016-08-20 07:58:03 -0600

    Can reuse saved searches

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,22 +11,47 @@ $(document).ready(function() {
         var searchItem = {
             "searchTerm": searchBox.val()
         };
-        $.ajax({
-            "url": 'http://localhost:9000/wikiview',
-            "method": "POST",
-            "data": JSON.stringify(searchItem),
-            "dataType": "json",
-            "contentType": "application/json"
-        }).done(function(data) {
-            searchBox.val('');
+        handleSearching(searchItem).done(function(data) {
+            resultsList.html('');
             data.forEach(function(result) {
                 resultsList.append(resultsTemplate(result));
             });
             savedSearches.append(savedSearchTemplate(searchItem));
+            searchBox.val('');
         }).fail(function(error) {
             console.log("There was an error", error);
-        });
+        });;
+    });
+
+    //Redo search with saved search item
+    $('.saved-searches').on('click', 'li .search-term', function(event) {
+        event.preventDefault();
+        var searchItem = {
+            "searchTerm": $(this).closest('li').attr('id')
+        };
+        handleSearching(searchItem).done(function(data) {
+            resultsList.html('');
+            searchBox.val('');
+            data.forEach(function(result) {
+                resultsList.append(resultsTemplate(result));
+            });
+        }).fail(function(error) {
+            console.log("There was an error", error);
+        });;
     });
     //Delegate action for deleting saved searches
-
+    $('.saved-searches').on('click', '.badge', function(event) {
+        event.stopPropagation();
+        $(this).closest('li').remove();
+    });
 });
+function handleSearching(searchItem) {
+    return $.ajax({
+        "url": 'http://localhost:9000/wikiview',
+        "method": "POST",
+        "data": JSON.stringify(searchItem),
+        "dataType": "json",
+        "contentType": "application/json"
+    });
+}
+
