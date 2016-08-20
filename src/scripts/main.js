19a235b351dfed244e1ab57463e35ead47b655a8
Greg Smith <crashingwaves@fea.st>
2016-08-19 20:21:39 -0600

    Save searches to list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,32 @@
-//Submit button handler
-$('#search-form').on('submit', function(event) {
-    event.preventDefault();
-    var searchItem = {
-        "searchTerm": $('#search-term').val()
-    };
-    $.ajax({
-        "url": 'http://localhost:9000/wikiview',
-        "method": "POST",
-        "data": JSON.stringify(searchItem),
-        "dataType": "json",
-        "contentType": "application/json"
-    }).done(function(data) {
-        var resultsList = $("#results-list"),
-            resultsTemplate = Handlebars.compile($('#result-template').html());
-        data.forEach(function(result) {
-            resultsList.append(resultsTemplate(result));
-        });
-    }).fail(function(error) {
-        console.log("There was an error", error);
-    });
-});
-//Delegate action for deleting saved searches
+$(document).ready(function() {
 
+    //Submit button handler
+    var searchBox = $('#search-term'),
+        resultsList = $('#results-list'),
+        savedSearches = $('.saved-searches'),
+        resultsTemplate = Handlebars.compile($('#result-template').html()),
+        savedSearchTemplate = Handlebars.compile($('#saved-search-template').html());
+    $('#search-form').on('submit', function(event) {
+        event.preventDefault();
+        var searchItem = {
+            "searchTerm": searchBox.val()
+        };
+        $.ajax({
+            "url": 'http://localhost:9000/wikiview',
+            "method": "POST",
+            "data": JSON.stringify(searchItem),
+            "dataType": "json",
+            "contentType": "application/json"
+        }).done(function(data) {
+            searchBox.val('');
+            data.forEach(function(result) {
+                resultsList.append(resultsTemplate(result));
+            });
+            savedSearches.append(savedSearchTemplate(searchItem));
+        }).fail(function(error) {
+            console.log("There was an error", error);
+        });
+    });
+    //Delegate action for deleting saved searches
+
+});
